e298631ccc729c3ab4133b790df282eb6d884590
Joel Brenstrum <joel.brenstrum@gmail.com>
2019-10-27 15:54:21 +1300

    Updates

---
+++ precache-manifest.640c5c7b79f27ce900481289e9a9dbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fb69dc0c565999c7cbc6a91260d13a",
+    "url": "/shortsorpants/index.html"
+  },
+  {
+    "revision": "ab4941c990d54abc4306",
+    "url": "/shortsorpants/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "9088fd7c459078084106",
+    "url": "/shortsorpants/static/css/main.3a5578be.chunk.css"
+  },
+  {
+    "revision": "ab4941c990d54abc4306",
+    "url": "/shortsorpants/static/js/2.791163ee.chunk.js"
+  },
+  {
+    "revision": "9088fd7c459078084106",
+    "url": "/shortsorpants/static/js/main.18afbc2e.chunk.js"
+  },
+  {
+    "revision": "714977425cdf7e4b1eec",
+    "url": "/shortsorpants/static/js/runtime-main.f0f7f037.js"
+  }
+]);
